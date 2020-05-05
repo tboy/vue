e47bfaee25890e148b05694ba60999237a723e79
fudehong <fudehong@qq.com>
2020-05-05 08:30:15 +0800

    #666

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">白市6</h3>
+        <h3 class="title">水果市场</h3>
       </div>
 
       <el-form-item prop="username">
